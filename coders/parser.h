f325de1dd6033834e345510e404cc0aa162ce8c9
harena-mihaja <mihaja2506@gmail.com>
2026-09-07 16:28:44 +0300

    feat: add struct for data representation

---
+++ coders/parser.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrahamal <hrahamal@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/07 16:07:49 by hrahamal          #+#    #+#             */
+/*   Updated: 2026/09/08 08:26:36 by hrahamal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+
+typedef struct s_config
+{
+	int		num_coders;
+	int		time_burnout;
+	int		time_compile;
+	int		time_debug;
+	int		time_refactor;
+	int		num_compiles;
+	int		dongle_cooldown;
+	char	*scheduler;
+}	t_config;
+
+#endif
