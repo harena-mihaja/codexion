1640b33f9107b30a1220280c39632c869c42da19
harena-mihaja <mihaja2506@gmail.com>
2026-09-08 14:32:23 +0300

    feat(validator): add various validator functions

--- coders/validator.c
+++ coders/validator.c
@@ -6,11 +6,12 @@
 /*   By: hrahamal <hrahamal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 13:38:43 by hrahamal          #+#    #+#             */
-/*   Updated: 2026/09/08 14:04:48 by hrahamal         ###   ########.fr       */
+/*   Updated: 2026/09/08 14:22:31 by hrahamal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/parser.h"
+#include "headers/utils.h"
 
 static int	is_num_valid(int n);
 static int	is_time_valid(long time);
@@ -18,14 +19,35 @@ static int	is_scheduler_valid(char *s);
 
 int	is_config_valid(t_config config)
 {
-	if (!is_num_valid(config.num_coders) ||
-		!is_num_valid(config.num_compiles) ||
-		!is_time_valid(config.time_burnout) ||
-		!is_time_valid(config.time_compile) ||
-		!is_time_valid(config.time_debug) ||
-		!is_time_valid(config.time_refactor) ||
-		!is_time_valid(config.dongle_cooldown) ||
-		!is_scheduler_valid(config.scheduler))
+	if (!is_num_valid(config.num_coders)
+		|| !is_num_valid(config.num_compiles)
+		|| !is_time_valid(config.time_burnout)
+		|| !is_time_valid(config.time_compile)
+		|| !is_time_valid(config.time_debug)
+		|| !is_time_valid(config.time_refactor)
+		|| !is_time_valid(config.dongle_cooldown)
+		|| !is_scheduler_valid(config.scheduler))
+		return (0);
+	return (1);
+}
+
+static int	is_num_valid(int n)
+{
+	if (n <= 0)
+		return (0);
+	return (1);
+}
+
+static int	is_time_valid(long time)
+{
+	if (time <= 0)
+		return (0);
+	return (1);
+}
+
+static int	is_scheduler_valid(char *s)
+{
+	if (strcmp(s, "fifo") && strcmp(s, "edf"))
 		return (0);
 	return (1);
 }
